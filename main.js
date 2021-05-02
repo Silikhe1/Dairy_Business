864e90457d3d9a3508b634374c4ccc16fb47f233
silikhe1 <silas.wanjala@student.moringaschool.com>
2021-05-02 16:57:44 +0300

    Displaying icome content none on click

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-function filterFunction() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
+const incomeBtn = document.querySelector(".right-btn");
+const pdtBtn = document.querySelector(".left-btn");
+var input, filter, table, tr, td, i, txtValue;
+table = document.getElementById("myTable");
+tr = table.getElementsByTagName("tr");
+
+const filterFunction = () => {
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
+
+      for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
       if (td) {
         txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter)>-1) // Tip: the indexOf() method returns the indexes at which a given element can be found in the array, or -1 if it is not present. In other words, it checks wether the text in filter is also present in txtvalue - our array.
+        if (txtValue.toUpperCase().indexOf(filter)>-1)
           
         {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
-          //tip: If displays set to "none" then nothing will be shown.
           
         }
       }
@@ -25,4 +25,16 @@ function filterFunction() {
   }
   
 
-  
+  incomeBtn.addEventListener("click", ()=>{
+      incomeBtn.classList.add("left-btn");
+      pdtBtn.classList.remove("left-btn");
+      pdtBtn.classList.add("right-btn");
+      table.style.display = "block"
+  })
+
+  pdtBtn.addEventListener("click", ()=>{
+    pdtBtn.classList.add("left-btn");
+    incomeBtn.classList.remove("left-btn");
+    pdtBtn.classList.add("right-btn");
+    table.style.display = "none"
+})
